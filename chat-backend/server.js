31b494b0f47e5a84a115e6d386261c00de9d549d
zhongqifeng <1121021693@qq.com>
2024-09-26 14:34:33 +0800

    init

---
+++ chat-backend/server.js
@@ -0,0 +1,35 @@
+/*
+ * @Description: 
+ * @Author: shanchuan
+ * @Date: 2024-09-26 14:15:44
+ * @LastEditors: 
+ * @LastEditTime: 2024-09-26 14:16:22
+ */
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server listening on port 4000');
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+<!--
+ * @Description: 
+ * @Author: shanchuan
+ * @Date: 2024-09-26 14:31:42
+ * @LastEditors: 
+ * @LastEditTime: 2024-09-26 14:31:51
+-->
+https://dev.to/jagroop2001/building-a-real-time-chat-application-with-socketio-express-reactjs-and-chakra-ui-vite-setup-5h5p
